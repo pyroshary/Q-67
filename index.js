e1551afdf9dbb2dd6beb28c8ac19b11da3f316c7
pyroshary <sheharyarghulamrasool@gmail.com>
2024-04-03 02:01:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+function numbersandstring(number1, number2) {
+    return number1 + Number(number2);
+}
+console.log(numbersandstring(5, "5"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+ function numbersandstring (number1: number , number2 : string): number{
+    return number1 + Number(number2);
+ }
+
+ console.log(numbersandstring(5,"5"));
